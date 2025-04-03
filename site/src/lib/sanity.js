0651d2ba7672cbba1d8a7a074e645c832b5a17cb
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2025-04-03 01:57:04 -0400

    add sanity

--- site/src/lib/sanity.js
+++ site/src/lib/sanity.js
@@ -4,5 +4,6 @@ export const client = createClient({
   projectId: 'f0k2uz7k',
   dataset: 'production',
   apiVersion: '2023-05-03',
-  useCdn: process.env.NODE_ENV === 'production'
+  useCdn: true,
+  perspective: 'published'
 }) 
